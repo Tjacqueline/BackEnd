1a3d2c27f5a5f999c07d8b9a3792a1076fa54b4a
Tjacqueline <vtamarajacqueline@gmail.com>
2022-10-31 17:43:58 -0300

    persona

--- src/main/java/com/portfolio/tjv/Entity/Persona.java
+++ src/main/java/com/portfolio/tjv/Entity/Persona.java
@@ -1,6 +1,5 @@
 package com.portfolio.tjv.Entity;
 
-import java.io.Serializable;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -9,10 +8,10 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 @Entity
-public class Persona implements Serializable {
+public class Persona {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private int id;
     
     @NotNull
     @Size(min = 1, max = 50, message = "no cumple con la longitud")
@@ -22,14 +21,26 @@ public class Persona implements Serializable {
     @Size(min = 1, max = 50, message = "no cumple con la longitud")
     private String apellido;
     
-    @Size(min = 1, max = 50, message = "no cumple con la longitud")
+    @NotNull
+    private String descripcion;
+    
     private String img;
 
-    public Long getId() {
+    public Persona() {
+    }
+
+    public Persona(String nombre, String apellido, String descripcion, String img) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.descripcion = descripcion;
+        this.img = img;
+    }
+
+    public int getId() {
         return id;
     }
 
-    public void setId(Long id) {
+    public void setId(int id) {
         this.id = id;
     }
 
@@ -49,6 +60,14 @@ public class Persona implements Serializable {
         this.apellido = apellido;
     }
 
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
     public String getImg() {
         return img;
     }
@@ -56,6 +75,6 @@ public class Persona implements Serializable {
     public void setImg(String img) {
         this.img = img;
     }
-    
+
     
 }
