3e5f3d58f34595c003a6e69b91f1a98746646631
Tjacqueline <vtamarajacqueline@gmail.com>
2022-10-31 13:56:27 -0300

    BackEnd separado del FrontEnd

---
+++ src/main/java/com/portfolio/tjv/Security/MainSecurity.java
@@ -0,0 +1,90 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.portfolio.tjv.Security;
+
+import com.portfolio.tjv.Security.Service.UserDetailsImpl;
+import com.portfolio.tjv.Security.jwt.JwtEntryPoint;
+import com.portfolio.tjv.Security.jwt.JwtTokenFilter;
+import java.util.Arrays;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class MainSecurity extends WebSecurityConfigurerAdapter{
+    @Autowired
+    UserDetailsImpl userDetailsServiceImpl;
+    @Autowired
+    JwtEntryPoint jwtEntryPoint;
+    
+    @Bean
+    public JwtTokenFilter jwtTokenFilter(){
+        return new JwtTokenFilter();
+    }
+    
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+  @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+
+        List<String> list1 = Arrays.asList(new String[]{"Authorization", "Cache-Control", "Content-Type"});
+        List<String> list2 = Arrays.asList(new String[]{"https://frontendtjv.web.app"});
+        List<String> list3 = Arrays.asList(new String[]{"GET", "POST", "PUT", "DELETE", "OPTIONS"});
+        List<String> list4 = Arrays.asList(new String[]{"Authorization"});
+
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowedHeaders(list1);
+        corsConfiguration.setAllowedOrigins(list2);
+        corsConfiguration.setAllowedMethods(list3);
+        corsConfiguration.setAllowCredentials(true);
+        corsConfiguration.setExposedHeaders(list4);
+
+        http.csrf().disable();
+        http.authorizeRequests().antMatchers("**").permitAll();
+        http.authorizeRequests().anyRequest().authenticated().and().httpBasic();
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.cors().configurationSource(request -> corsConfiguration);
+        http.addFilterBefore(jwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
+
+    }
+
+    
+    @Override
+    protected AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManager();
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsServiceImpl).passwordEncoder(passwordEncoder());
+    }
+    
+    
+    
+}
